d70f3b8a5b58090ecafa94b0243bfe3390dd4fdb
anomen <ludek.h@gmail.com>
2017-01-11 10:39:26 +0100

    AesCrypt - update Markup setup

--- AesCrypt/aescrypt.php
+++ AesCrypt/aescrypt.php
@@ -3,14 +3,14 @@
 /*
     AesCrypt
 
-    Copyright 2011-2013 Anomen (ludek_h@seznam.cz)
+    Copyright 2011-2017 Anomen (ludek_h@seznam.cz)
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published
     by the Free Software Foundation; either version 2 of the License, or
     (at your option) any later version.
 */
 
-$RecipeInfo['AesCrypt']['Version'] = '2013-01-11';
+$RecipeInfo['AesCrypt']['Version'] = '2017-01-14';
 
 SDV($AesCryptKDF, 'sha256_dup');
 SDV($AesCryptKDFJSCode, "
@@ -115,10 +115,17 @@ function aescryptMarkup($ciphertext)
     return $res;
 }
 
-Markup('aescrypt',
-       "<[=",
-       "/\\Q$AesCryptCipherToken\\E\\s*(.*?)\\s*\\Q$AesCryptEndToken\\E/se",
-       "aescryptMarkup('$1')");
+if (function_exists('Markup_e')) {
+  Markup_e('aescrypt',
+       ">block",
+       "/\\Q$AesCryptCipherToken\\E\\s*([a-zA-Z0-9\\/\\+]+=*)\\s*\\Q$AesCryptEndToken\\E/s",
+       "aescryptMarkup(\$m[1])");
+} else {
+  Markup('aescrypt',
+       ">block",
+       "/\\Q$AesCryptCipherToken\\E\\s*([a-zA-Z0-9\\/\\+]+=*)\\s*\\Q$AesCryptEndToken\\E/se",
+       "aescryptMaxrkup('$1')");
+}
 
 if ($action == 'edit') {
 
