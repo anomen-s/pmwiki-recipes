84d6e6299fc5da7f5d898e3705db6f784b16214b
anomen <ludek.h@gmail.com>
2018-05-23 13:34:54 +0200

    WikiCalendar - fork of https://pmwiki.org/wiki/Cookbook/WikiCalendar

---
+++ WikiCalendar/wikilog-i18n-en.php
@@ -0,0 +1,30 @@
+<?php
+
+# wikilog English version
+
+$monthnames = array('January','February','March','April','May','June','July','August', 'September','October','November','December'); $shortdaynames = array('Sun','Mon','Tue','Wed','Thu','Fri','Sat','Sun');$longdaynames  = 
+array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
+$noentries = 'No entries for';
+$today     = 'Today';
+$thisweek  = array('Yesterday',$today,'Tomorrow');
+$edit_text = 'edit';
+$below  = 'See below.';
+$period = 'period';
+$sequence = array('Newest first','Oldest first');
+$publish  = 'Publish';
+$default_date_text = 'From the wiki calendar';
+$WikilogHomeText   = 'Calendar';
+$DefaultCalendarTitle = 'Post a new story for selected date';
+
+# Submit form text
+$DateText = 'Date';
+$HeadlineText = 'Headline';
+$SubpageText  = 'Subpage';
+$StoryText = 'Story';
+$AuthorText = 'Author';
+$PostText = ' Post ';
+$ResetText = 'Reset';
+$AccessCodeText = 'Enter code';
+$posted_by = 'Posted $Date by $Author';
+
+?>
